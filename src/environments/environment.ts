1cea21f012b26b0a8b7e0695037120d7822c398b
SarthakHarbinger <139337367+SarthakHarbinger@users.noreply.github.com>
2023-07-20 14:43:52 +0530

    Boiler plate code

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'course-website-df372',
+    appId: '1:270866309204:web:ba5e438a18cbe5d0b2cdc1',
+    storageBucket: 'course-website-df372.appspot.com',
+    apiKey: 'AIzaSyAhEckO6nByfvKAZOWAHz0HkQI4Aqttrzc',
+    authDomain: 'course-website-df372.firebaseapp.com',
+    messagingSenderId: '270866309204',
+  },
+};
